ec3e422dfc5a864f4067a63a0d2463e3c6bd6a1c
MaurizioLeombruni <maurizio.leombruni@gmail.com>
2023-01-10 15:19:32 +0100

    Creazione elementi del database.

---
+++ WebApplicationTest/Database/PizzeriaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationTest.Models;
+
+namespace WebApplicationTest.Database
+{
+    public class PizzeriaContext : DbContext
+    {
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer("Data Source = localhost; Database = PizzeriaV1; " + "Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
--- WebApplicationTest/Models/Pizza.cs
+++ WebApplicationTest/Models/Pizza.cs
@@ -1,12 +1,28 @@
-﻿namespace WebApplicationTest.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplicationTest.Models
 {
     public class Pizza
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Image { get; set; }
-        public string Price { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public string? Name { get; set; }
+
+        [Column(TypeName = "text")]
+        public string? Description { get; set; }
+
+        [Column(TypeName = "varchar(500)")]
+        public string? Image { get; set; }
+
+        [Column(TypeName = "varchar(10)")]
+        public string? Price { get; set; }
+
+        public Pizza()
+        {
+
+        }
 
         public Pizza(int id, string name, string description, string image, string price)
         { 
