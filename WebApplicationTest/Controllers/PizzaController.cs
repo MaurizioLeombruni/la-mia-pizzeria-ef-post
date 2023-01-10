eece70cb969c7e98d5dcc31e57e9e9a36eaf23ce
MaurizioLeombruni <maurizio.leombruni@gmail.com>
2023-01-10 16:06:35 +0100

    Aggiunta gestione del form e del database al Controller.

--- WebApplicationTest/Controllers/PizzaController.cs
+++ WebApplicationTest/Controllers/PizzaController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using WebApplicationTest.Database;
 using WebApplicationTest.Models;
 using WebApplicationTest.Utilities;
 
@@ -8,24 +11,51 @@ namespace WebApplicationTest.Controllers
     {
         public IActionResult Index()
         {
-            List<Pizza> pizzaList = PizzaData.ConstructPizzas();
-
-            return View("Index", pizzaList);
+            using (PizzeriaContext db = new PizzeriaContext())
+            {
+                List<Pizza> listaPizze = db.Pizzas.ToList();
+                return View("Index", listaPizze);
+            }
         }
 
         public IActionResult Details(int id)
         {
-            List<Pizza> pizzaList = PizzaData.ConstructPizzas();
-
-            foreach(Pizza element in pizzaList)
+            using (PizzeriaContext db = new PizzeriaContext())
             {
-                if(element.Id == id)
+                Pizza pizzaDaCercare = db.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+                if (pizzaDaCercare != null)
                 {
-                    return View(element);
+                    return View(pizzaDaCercare);
                 }
+
+                return NotFound("Something went wrong tralalala");
             }
 
-            return NotFound("Something went wrong tralalala");
+        }
+
+        [HttpGet]
+        public IActionResult GenerateForm()
+        {
+            return View("GenerateForm");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult GenerateForm(Pizza formData)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("GenerateForm", formData);
+            }
+
+            using(PizzeriaContext db = new PizzeriaContext())
+            {
+                db.Pizzas.Add(formData);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
